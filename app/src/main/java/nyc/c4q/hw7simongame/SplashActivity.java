1b490af370b76ff09f130e1e8ae3996cd5f1e056
Volha Kaleda <volhakaleda@ac.c4q.nyc>
2017-10-31 00:53:47 -0400

    splash activity

---
+++ app/src/main/java/nyc/c4q/hw7simongame/SplashActivity.java
@@ -0,0 +1,29 @@
+package nyc.c4q.hw7simongame;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, GameActivity.class);
+                startActivity(intent);
+
+            }
+        };
+
+        handler.postDelayed(r, 15000);
+        Toast.makeText(getApplicationContext(), "rules", Toast.LENGTH_LONG).show();
+    }
+}
